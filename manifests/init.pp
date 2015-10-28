6a8d23dfb7b9c6499f0026f5967621860842a5e7
Steve Huff <steve.huff@runkeeper.com>
2015-10-28 12:36:33 -0400

    ready for first test

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,6 +9,39 @@ class rk_datahub (
 ) {
   validate_re($tier, '^(production|staging)$', "\$tier must be 'production' or 'staging', not '${tier}'.")
 
+  $user_key = $user_keys[$tier]
 
+  $datahub_remote_pkg = "${repo}/DataHub_${version}.deb"
+  $datahub_local_pkg = "/root/datahub.deb"
+
+
+
+  wget::fetch { $datahub_remote_pkg:
+    destination => $datahub_local_pkg,
+    cachedir    => '/tmp',
+    timeout     => 0,
+    verbose     => true,
+  } ->
+
+  package { 'datahub':
+    ensure   => installed,
+    provider => 'dpkg',
+    source   => $datahub_local_pkg,
+  } ->
+
+  file { '/etc/datahub/datahub.config':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('rk_datahub/datahub.config.erb'),
+  } ~>
+
+  service { 'datahub':
+    ensure     => stopped,
+    enabled    => true,
+    hasstatus  => true,
+    hasrestart => true,
+  }
 
 }
